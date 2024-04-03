b2976cdd1c0ef6679cbedb1224cf6b26d152a946
AckimJnr <acklongwe@gmail.com>
2024-04-03 21:00:00 +0200

    Update fifo cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3
 """FIFOCache Module"""
 from base_caching import BaseCaching
-from collections import deque
 
 
 class FIFOCache(BaseCaching):
-    """
-    Implements FIFO Cache
-    """
+    """FIFO caching class"""
+
     def __init__(self):
+        """Constructor"""
         super().__init__()
-        self.queue = deque()
 
     def put(self, key, item):
-        """
-        Add an item to the cache
-        """
-        if key is not None or item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                key_removed = self.queue.popleft()
-                del self.cache_data[key_removed]
-                print("Discard: {}".format(key_removed))
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                first_item = next(iter(self.cache_data))
+                del self.cache_data[first_item]
+                print("DISCARD:", first_item)
+
         self.cache_data[key] = item
-        self.queue.append(key)
 
     def get(self, key):
-        """Get an item from the cache"""
-        if key is not None:
-            try:
-                return self.cache_data.get(key)
-            except KeyError:
-                return None
+        """Retrieve an item from cache"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
