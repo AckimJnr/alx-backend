311996b638c36595473f4cba8ae3e107874a7c57
AckimJnr <acklongwe@gmail.com>
2024-04-03 06:04:13 +0200

    Add basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""BasicCacheModule
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Implement basic cache"""
+
+    def put(self, key, value):
+        if key is None or value is None:
+            pass
+        else:
+            self.cache_data[key] = value
+
+    def get(self, key):
+        """
+        get item from cache
+        """
+        if key is not None:
+            try:
+                return self.cache_data.get(key)
+            except KeyError:
+                return None
