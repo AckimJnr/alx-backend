ae5cfbe98c68e78d04f13a366a31765e7ca0f730
AckimJnr <acklongwe@gmail.com>
2024-04-09 19:55:26 +0200

    Add docstring

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,8 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Root route of our application
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
+    """
+    If main module execute
+    """
     app.run(host='0.0.0.0', port='5000')
