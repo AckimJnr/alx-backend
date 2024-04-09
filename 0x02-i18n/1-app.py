f1e1b60b5521069d4d43abead939e6eb60b949b9
AckimJnr <acklongwe@gmail.com>
2024-04-09 21:37:00 +0200

    Add root route

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -18,3 +18,18 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
+
+@app.route("/")
+def index() -> str:
+    """
+    Root route of our application
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    """
+    If main module execute
+    """
+    app.run(host='0.0.0.0', port='5000')
